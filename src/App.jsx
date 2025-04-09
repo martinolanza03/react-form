ed77e862fe9275df092476ab2201e0aa83e734f9
martinolanza03 <lanzamartino3@gmail.com>
2025-04-09 15:53:27 +0200

    error: error send article

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
 import articles from './assets/articles'
+import { useState } from 'react'
+
 
 function App() {
 
+  const [myArticle, setArticle] = useState(articles)
+  let [newArticle, setNewArticle] = useState('');
+
+  const sendForm = event => {
+    event.preventDefault();
+    setArticle([...myArticle, newArticle]);
+    setNewArticle('');
+
+
+    console.log("L'articolo è stato aggiunto");
+    console.log(articles);
+  }
+
   return (
     <>
-      <ul>{articles.map(article => <li>{article.title}</li>)}</ul>
+      <form onSubmit={sendForm}>
+        <input type="text" placeholder='Inserisci un nuovo articolo' value={newArticle} onChange={event => { setNewArticle(event.target.value) }} />
+        <button>Invia</button>
+      </form>
+
+      <ul>{myArticle.map((article, i) => <li key={i}>{article.title}</li>)}</ul>
+
 
     </>
   )
