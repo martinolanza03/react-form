51638394fa3158ac865383778730edcaa16f7d5b
martinolanza03 <lanzamartino3@gmail.com>
2025-04-09 15:04:12 +0200

    feat: change name key

--- src/assets/articles.js
+++ src/assets/articles.js
@@ -1,28 +1,28 @@
 const articles = [
     {
         "id": 1,
-        "titolo": "Le meraviglie dello spazio",
-        "descrizione": "Un viaggio tra stelle e galassie per scoprire i segreti dell'universo."
+        "title": "Le meraviglie dello spazio",
+        "description": "Un viaggio tra stelle e galassie per scoprire i segreti dell'universo."
     },
     {
         "id": 2,
-        "titolo": "L'arte della cucina italiana",
-        "descrizione": "Un approfondimento sui piatti tipici della tradizione italiana e le loro origini."
+        "title": "L'arte della cucina italiana",
+        "description": "Un approfondimento sui piatti tipici della tradizione italiana e le loro origini."
     },
     {
         "id": 3,
-        "titolo": "Tecnologie emergenti nel 2025",
-        "descrizione": "Uno sguardo alle innovazioni che stanno rivoluzionando il nostro modo di vivere e lavorare."
+        "title": "Tecnologie emergenti nel 2025",
+        "description": "Uno sguardo alle innovazioni che stanno rivoluzionando il nostro modo di vivere e lavorare."
     },
     {
         "id": 4,
-        "titolo": "I benefici della meditazione",
-        "descrizione": "Scopri come la meditazione può migliorare il benessere mentale e fisico."
+        "title": "I benefici della meditazione",
+        "description": "Scopri come la meditazione può migliorare il benessere mentale e fisico."
     },
     {
         "id": 5,
-        "titolo": "Viaggi avventurosi nel mondo",
-        "descrizione": "Esploriamo le destinazioni più affascinanti per chi ama l'avventura."
+        "title": "Viaggi avventurosi nel mondo",
+        "description": "Esploriamo le destinazioni più affascinanti per chi ama l'avventura."
     }
 ]
 
