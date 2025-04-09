357390841f90942718fe6a18776d580f1829dc7f
martinolanza03 <lanzamartino3@gmail.com>
2025-04-09 14:59:48 +0200

    feat: create articole json

---
+++ src/assets/article.js
@@ -0,0 +1,29 @@
+const article = [
+    {
+        "id": 1,
+        "titolo": "Le meraviglie dello spazio",
+        "descrizione": "Un viaggio tra stelle e galassie per scoprire i segreti dell'universo."
+    },
+    {
+        "id": 2,
+        "titolo": "L'arte della cucina italiana",
+        "descrizione": "Un approfondimento sui piatti tipici della tradizione italiana e le loro origini."
+    },
+    {
+        "id": 3,
+        "titolo": "Tecnologie emergenti nel 2025",
+        "descrizione": "Uno sguardo alle innovazioni che stanno rivoluzionando il nostro modo di vivere e lavorare."
+    },
+    {
+        "id": 4,
+        "titolo": "I benefici della meditazione",
+        "descrizione": "Scopri come la meditazione può migliorare il benessere mentale e fisico."
+    },
+    {
+        "id": 5,
+        "titolo": "Viaggi avventurosi nel mondo",
+        "descrizione": "Esploriamo le destinazioni più affascinanti per chi ama l'avventura."
+    }
+]
+
+export default article
